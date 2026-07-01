9a551cd1a44e4c8458e40f4ba9911d15049ddf83
aspatil0 <aspatil2904@gmail.com>
2026-07-01 15:26:53 +0530

    Update package files and routes

--- Frontend/src/Routes/AppRoute.jsx
+++ Frontend/src/Routes/AppRoute.jsx
@@ -7,22 +7,42 @@ import Info from "../Pages/Info/Info";
 import PYQ from "../Pages/PYQ/PYQ";
 import Syllabus from "../Pages/Syllabus/Syllabus";
 import Resources from "../Pages/Resources/Resources";
-import Profile from "../Pages/Profile/Profile";
+import ProfileLayout from "../Pages/Profile/Profile";
 import ContactUs from "../Pages/ContactUs/ContactUs";
+
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
+import ForgotPass from "../Pages/ForgotPass";
+import Dashboard from "../Pages/Dashboard";
+import ProfileSetup from "../Pages/Profile";
 
 export default function AppRoute() {
     return (
         <Routes>
-            <Route path="/" element={<SelectYear />} />
+            {/* Auth routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgotpass" element={<ForgotPass />} />
+
+            {/* Main landing */}
+            <Route path="/" element={<Home />} />
+
+            {/* Dashboard & Profile Setup */}
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile-setup" element={<ProfileSetup />} />
+
+            {/* Year selection */}
+            <Route path="/select-year" element={<SelectYear />} />
             <Route path="/select-department/:year" element={<SelectDepartment />} />
 
+            {/* Year layout routes */}
             <Route path="/:year" element={<MainLayout />}>
                 <Route path="home" element={<Home />} />
                 <Route path="info" element={<Info />} />
                 <Route path="pyq" element={<PYQ />} />
                 <Route path="syllabus" element={<Syllabus />} />
                 <Route path="resources" element={<Resources />} />
-                <Route path="profile" element={<Profile />} />
+                <Route path="profile" element={<ProfileLayout />} />
                 <Route path="contactus" element={<ContactUs />} />
             </Route>
 
@@ -32,7 +52,7 @@ export default function AppRoute() {
                 <Route path="pyq" element={<PYQ />} />
                 <Route path="syllabus" element={<Syllabus />} />
                 <Route path="resources" element={<Resources />} />
-                <Route path="profile" element={<Profile />} />
+                <Route path="profile" element={<ProfileLayout />} />
                 <Route path="contactus" element={<ContactUs />} />
             </Route>
         </Routes>
